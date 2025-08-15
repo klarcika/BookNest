1602a8c6b0ba6d56e17e28f33cd8b4b45294e342
KLARA KIRBIS <klara.kirbis@student.um.si>
2025-08-15 21:56:31 +0200

    popravek

--- frontend/src/pages/AdminPage.jsx
+++ frontend/src/pages/AdminPage.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { jwtDecode } from 'jwt-decode';
-import { bookApi, reviewApi } from './api';
+import { bookApi, reviewApi } from '../api';
 
 const AdminPage = () => {
     const [newBook, setNewBook] = useState({
